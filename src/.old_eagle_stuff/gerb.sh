7bb2f99851dbaa1fc91d426bfda4b54af280b311
Justin Hsu <justinhsu845@berkeley.edu>
2023-08-29 03:44:50 -0700

    Moved old eagle src files into hidden directory

---
+++ src/.old_eagle_stuff/gerb.sh
@@ -0,0 +1,8 @@
+cd ..
+mkdir newgerb
+cp -rt newgerb $(find . -name "*-gerb" -type d)
+zip -r newgerb.zip newgerb
+mv newgerb.zip gerbs
+rm -rf newgerb
+rm -rf $(find . -name "*-gerb" -type d)
+echo "newgerb.zip is done"
