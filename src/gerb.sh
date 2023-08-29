0d2fff119dbf881977d4ccb321c6899b7f69aaa8
Justin Hsu <justinhsu845@berkeley.edu>
2023-08-29 03:45:48 -0700

    Moved some fabrication files and libraries

--- src/gerb.sh
+++
@@ -1,8 +0,0 @@
-cd ..
-mkdir newgerb
-cp -rt newgerb $(find . -name "*-gerb" -type d)
-zip -r newgerb.zip newgerb
-mv newgerb.zip gerbs
-rm -rf newgerb
-rm -rf $(find . -name "*-gerb" -type d)
-echo "newgerb.zip is done"
